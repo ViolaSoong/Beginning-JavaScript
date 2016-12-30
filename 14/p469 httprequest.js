e0ae4d1bfbd0dccfd703d5e38f8148e5a285cd78
ViolaSoong <violasoong@163.com>
2016-12-30 18:27:07 +0800

    XMLHttpRequest

---
+++ 14/p469 httprequest.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Administrator on 2016/12/30.
+ */
+
+function HttpRequest(url, callback) {
+    this.request = new XMLHttpRequest();
+    this.request.open("GET", url);
+
+    var tempRequest = this.request;
+
+    function reqReadyStateChange() {
+        if (tempRequest.readyState == 4) {
+            if (tempRequest.status == 200) {
+                callback(tempRequest, responseText);
+            } else {
+                alert("An error occurred trying to contact the server.");
+            }
+        }
+    }
+
+    this.request.onreadystatechange = reqReadyStateChange;
+}
+
+HttpRequest.prototype.send = function () {
+    this.request.send(null);
+};
